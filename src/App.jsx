317bbd78b9f9de973ba4eed6170be9f29ca526a2
subintm <subintmtm70@gmail.com>
2023-12-27 12:16:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter.jsx'
+
+function App() {
+
+
+  return (
+    <div style={{height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center'> 
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
